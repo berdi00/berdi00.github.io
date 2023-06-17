cb618e5d96e766cc45ac4d2d408749f722e5bab1
berdi00 <berdinazarov731@gmail.com>
2023-06-17 18:22:33 +0500

    new project

--- src/text.js
+++
@@ -1,3 +0,0 @@
-const about = `✋ I'm Berdimyrat Nazarov, sophomore student in TITU. I live in Ashgabat and like building web applications in JS. I got inspired by "dorota1997.github.io" in this portfolio template.`;
-
-export default about;
